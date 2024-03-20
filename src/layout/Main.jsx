a2fb2d5892f8b4a544297c44dc160d23c34ff98d
skrimenos <ozan_cankal@outlook.com>
2024-03-20 15:30:19 +0300

    fix

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
 
-const API_KEY = process.env.RREACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
   state = {
